52c6496c48f4940b6b571d90276db366573da33a
FrissESC <erwin.schuijtvlot@friss.eu>
2017-03-29 08:52:41 +0200

    initial commit

---
+++ R/functions.R
@@ -0,0 +1,18 @@
+#' Function 1
+#'
+#'
+#' @export
+#' @param data frame with data to classify
+function1 <- function(){
+  return (runif(1))
+}
+
+#' Function 1
+#'
+#'
+#' @export
+#' @param data frame with data to classify
+function2 <- function(){
+  return (runif(1))*100
+}
+
